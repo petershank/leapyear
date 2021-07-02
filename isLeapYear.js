e51f52c23de225ff29c056c48dea1cfd6f56b5df
Peter Shank <peter@shank.com>
2021-07-01 23:37:10 -0700

    update isLeapYear.test.js

--- isLeapYear.js
+++ isLeapYear.js
@@ -10,8 +10,10 @@ function isCenturyException(k) {
   return isDivisibleBy(k, 100) && !isDivisibleBy(k, 400);
 }
 
-function isLeapYear(n, factor) {
-  return isCenturyException(k);
+function isLeapYear(n) {
+  return isCenturyException(n);
 }
+
+// console.log(isDivisibleBy(2400, 100) && !isDivisibleBy(2400, 400));
 
 module.exports = isLeapYear;
